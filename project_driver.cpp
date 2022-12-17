785c6356142f14d0f5663cc26658505fcb276c7c
Naveen A.B <53806497+RNA3210d@users.noreply.github.com>
2022-12-17 14:41:27 +0530

    UI buck

--- project_driver.cpp
+++ project_driver.cpp
@@ -1,6 +1,19 @@
 #include "project_driver.h"
 #include "./ui_project_driver.h"
-
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <QString>
+//#include <QQmlContext>
+#include <unistd.h>
+#include <ios>
+#include <fstream>
+#include <string>
+#include <QFileDialog>
+#include <QProcess>
+#include <QFile>
+#include <QTextStream>
+using namespace std;
 project_driver::project_driver(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::project_driver)
